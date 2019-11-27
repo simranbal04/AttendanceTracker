53a2b5a07f0122f4a821c93b8c692eacfa677228
Simranbal04 <simranbal01@gmail.com>
2019-11-27 01:00:29 -0500

    User Login

---
+++ app/src/main/java/com/example/attendancetaker/MainActivity.java
@@ -0,0 +1,69 @@
+package com.example.attendancetaker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button loginButton,cancelButton;
+    EditText edusername,edpassword;
+
+    TextView tx1;
+    int counter = 3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        loginButton = (Button)findViewById(R.id.button);
+        cancelButton = (Button)findViewById(R.id.button2);
+
+        edusername = (EditText)findViewById(R.id.edusername);
+        edpassword = (EditText)findViewById(R.id.edpassword);
+
+        tx1 = (TextView)findViewById(R.id.textView3);
+        tx1.setVisibility(View.GONE);
+
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                if (edusername.getText().toString().equals("123") &&  edpassword.getText().toString().equals("123"))
+                {
+                    Toast.makeText(getApplicationContext(),"Redirecting",Toast.LENGTH_SHORT).show();
+                }
+
+                else {
+                    Toast.makeText(getApplicationContext(),"Wrong Credentials",Toast.LENGTH_SHORT).show();
+                    tx1.setVisibility(View.VISIBLE);
+                    tx1.setBackgroundColor(Color.RED);
+                    counter--;
+                    tx1.setText(Integer.toString(counter));
+
+                    if (counter == 0 )
+                    {
+                        loginButton.setEnabled(false);
+                    }
+
+                }
+            }
+        }) ;
+
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+}
+
